029bbc55803f8aa560e52686ce2f3826ab0c7c5a
aditi-pujar <85410993+aditi-pujar@users.noreply.github.com>
2023-09-10 23:26:44 -0500

    function that creates LEUP dataset file structure
    this function is called within the main_run function

---
+++ LEUP_simulation_results/Generating_Datasets/LEUP/create_LEUP_dataset_file_structure.m
@@ -0,0 +1,29 @@
+function create_LEUP_dataset_file_structure()
+
+% For beta > 0
+mkdir ../Datasets/LEUP/datasets/square/r=1 
+mkdir ../Datasets/LEUP/datasets/square/r=2
+mkdir ../Datasets/LEUP/datasets/square/r=3 
+mkdir ../Datasets/LEUP/datasets/square/r=4 
+mkdir ../Datasets/LEUP/datasets/square/r=5 
+mkdir ../Datasets/LEUP/datasets/square/r=6 
+mkdir ../Datasets/LEUP/datasets/square/r=7 
+mkdir ../Datasets/LEUP/datasets/square/r=8 
+mkdir ../Datasets/LEUP/datasets/square/r=9
+mkdir ../Datasets/LEUP/datasets/square/r=10 
+
+% For beta < 0
+mkdir ../Datasets/LEUP/radial/r=1 
+mkdir ../Datasets/LEUP/radial/r=2
+mkdir ../Datasets/LEUP/radial/r=3 
+mkdir ../Datasets/LEUP/radial/r=4 
+mkdir ../Datasets/LEUP/radial/r=5 
+mkdir ../Datasets/LEUP/radial/r=6 
+mkdir ../Datasets/LEUP/radial/r=7 
+mkdir ../Datasets/LEUP/radial/r=8 
+mkdir ../Datasets/LEUP/radial/r=9 
+mkdir ../Datasets/LEUP/radial/r=10
+
+% For different neighborhoods
+
+mkdir ../Datasets/LEUP/neighborhoods
